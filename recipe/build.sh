d32c7a6ad97b3de6c1f4f258a18fbcaf564b4aaf
Tobias Fischer <info@tobiasfischer.info>
2026-06-19 16:23:17 +1000

    Update build.sh to handle CUDA_HOME for CUDA 12.0
    Set CUDA_HOME to BUILD_PREFIX if not defined for CUDA 12.0.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+if [[ -z "${CUDA_HOME+x}" ]]; then
+  # `$CUDA_HOME` was set for CUDA 11.x and earlier.
+  # Must be using CUDA 12.0+. So set to `$BUILD_PREFIX`.
+  # This is needed to find `nvcc` and `cudart`.
+  export CUDA_HOME="${BUILD_PREFIX}"
+fi
+
 if [[ ${cuda_compiler_version} == 12.0 ]]; then
     export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
 elif [[ ${cuda_compiler_version} == 12.6 ]]; then
